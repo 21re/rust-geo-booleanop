b60d42cc6c488a0f609be15828a45df2ca086176
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-01-28 23:39:05 +0100

    improve construction of contours

--- lib/src/boolean/connect_edges.rs
+++ lib/src/boolean/connect_edges.rs
@@ -100,14 +100,39 @@ pub struct Contour<F>
 where
     F: Float
 {
+    /// Raw coordinates of contour
     pub points: Vec<Coordinate<F>>,
+    /// Contour IDs of holes if any.
     pub hole_ids: Vec<i32>,
+    /// Contour ID of parent if this contour is a hole.
+    pub hole_of: Option<i32>,
+    /// Depth of the contour. Since the geo data structures don't store depth information,
+    /// this field is not strictly necessary to compute. But it is very cheap to compute,
+    /// so we can add it and see if it has relevance in the future.
+    pub depth: i32,
+}
+
+impl<F> Contour<F>
+where
+    F: Float
+{
+    pub fn new(hole_of: Option<i32>, depth: i32) -> Contour<F> {
+        Contour {
+            points: Vec::new(),
+            hole_ids: Vec::new(),
+            hole_of: hole_of,
+            depth: depth,
+        }
+    }
+
     /// The semantics of `is_exterior` are in the sense of an exterior ring of a polygon
     /// in GeoJSON. This is not to be confused with "external contour" as used in the
     /// Martinez paper (which refers to contours that are not included in any of the
     /// other polygon contours). `is_exterior` is true for all outer contours, not just
     /// the outermost.
-    pub is_exterior: bool,
+    pub fn is_exterior(&self) -> bool {
+        self.hole_of.is_none()
+    }
 }
 
 
@@ -159,51 +184,48 @@ where
     let mut result: Vec<Contour<F>> = Vec::new();
     let mut processed: HashSet<i32> = HashSet::new();
 
-    let mut depth: HashMap<i32, i32> = HashMap::new();
-    let mut hole_of: HashMap<i32, i32> = HashMap::new();
-
     for i in 0..(result_events.len() as i32) {
         if processed.contains(&i) {
             continue;
         }
-        let mut contour = Contour{
-            points: Vec::new(),
-            hole_ids: Vec::new(),
-            is_exterior: true,
-        };
 
         let contour_id = result.len() as i32;
-        println!("\n *** Adding contour id {}", contour_id);
-        depth.insert(contour_id, 0);
-        hole_of.insert(contour_id, -1);
 
-        if let Some(prev_in_result) = result_events[i as usize].get_prev_in_result() {
+        // This logic implements the 4 cases of parent contours from Fig. 4 in the Martinez paper.
+        let mut contour = if let Some(prev_in_result) = result_events[i as usize].get_prev_in_result() {
+            // Note that it is valid to query the "previous in result" for its output contour id,
+            // because we must have already processed it (i.e., assigned an output contour id)
+            // in an earlier iteration, otherwise it wouldn't be possible that it is "previous in
+            // result".
             let lower_contour_id = prev_in_result.get_output_contour_id();
-            println!("Inferring information from lower_contour_id = {} with result transition = {:?}", lower_contour_id, prev_in_result.get_result_transition());
-            println!("{:?}", prev_in_result.point);
-            println!("{:?}", prev_in_result.get_other_event().unwrap().point);
             if prev_in_result.get_result_transition() == ResultTransition::OutIn {
-                // We are inside, let's check if the thing below us is an exterior contour or just
-                // another hole.
-                if result[lower_contour_id as usize].is_exterior {
-                    result[lower_contour_id as usize].hole_ids.push(contour_id);
-                    hole_of.insert(contour_id, lower_contour_id);
-                    depth.insert(contour_id, depth[&lower_contour_id] + 1);
-                    contour.is_exterior = false;
-                    println!("Marking contour as hole of {} with depth {}", lower_contour_id, depth[&contour_id]);
-                } else {
-                    let parent_contour_id = hole_of[&lower_contour_id];
+                // We are inside. Now we have to check if the thing below us is an another hole
+                // or an exterior contour.
+                let lower_contour = &result[lower_contour_id as usize];
+                if let Some(parent_contour_id) = lower_contour.hole_of {
+                    // The lower contour is a hole => Connect the new contour as a hole to its parent,
+                    // and use same depth.
                     result[parent_contour_id as usize].hole_ids.push(contour_id);
-                    hole_of.insert(contour_id, parent_contour_id);
-                    depth.insert(contour_id, depth[&lower_contour_id]);
-                    contour.is_exterior = false;
-                    println!("Transitively marking contour as hole of {} via {} with depth {}", parent_contour_id, lower_contour_id, depth[&contour_id]);
+                    let hole_of = Some(parent_contour_id);
+                    let depth = result[lower_contour_id as usize].depth;
+                    Contour::new(hole_of, depth)
+                } else {
+                    // The lower contour is an exterior contour => Connect the new contour as a hole,
+                    // and increment depth.
+                    result[lower_contour_id as usize].hole_ids.push(contour_id);
+                    let hole_of = Some(lower_contour_id);
+                    let depth = result[lower_contour_id as usize].depth + 1;
+                    Contour::new(hole_of, depth)
                 }
             } else {
-                contour.is_exterior = true;
-                println!("Keeping contour as external");
+                // We are outside => this contour is an exterior contour of same depth.
+                let depth = result[lower_contour_id as usize].depth;
+                Contour::new(None, depth)
             }
-        }
+        } else {
+            // There is no lower/previous contour => this contour is an exterior contour of depth 0.
+            Contour::new(None, 0)
+        };
 
         let orig_pos = i; // Alias just for clarity
         let mut pos = i;
