78d48c943c3f8547e6b07e8978ad940f25d6618d
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-03-21 11:00:08 +0100

    get rid of type casts

--- lib/src/boolean/connect_edges.rs
+++ lib/src/boolean/connect_edges.rs
@@ -46,35 +46,34 @@ where
     result_events
 }
 
-fn build_vertex_iteration_order<T, I, L>(data: &[T], is_identical: I, is_left: L) -> Vec<usize>
+fn precompute_iteration_order<T, I, L>(data: &[T], is_identical: I, is_left: L) -> Vec<usize>
 where
     I: Fn(&T, &T) -> bool,
     L: Fn(&T) -> bool,
 {
     let mut map = vec![0; data.len()];
 
-    let mut i = 0 as i64;
+    let mut i = 0;
+    while i < data.len() {
+        let x_ref = &data[i];
 
-    println!("\n{}", data.len());
-
-    while i < data.len() as i64 {
-        let x_ref = &data[i as usize];
-
+        // Find index range of R events
         let r_from = i;
-        while i < data.len() as i64 && is_identical(x_ref, &data[i as usize]) && !is_left(&data[i as usize]) {
+        while i < data.len() && is_identical(x_ref, &data[i]) && !is_left(&data[i]) {
             i += 1;
         }
-        let r_upto_exclusive = i; // - 1;
+        let r_upto_exclusive = i;
 
+        // Find index range of L event
         let l_from = i;
-        while i < data.len() as i64 && is_identical(x_ref, &data[i as usize]) {
-            debug_assert!(is_left(&data[i as usize]));
+        while i < data.len() && is_identical(x_ref, &data[i]) {
+            debug_assert!(is_left(&data[i]));
             i += 1;
         }
-        let l_upto_exclusive = i; // - 1;
+        let l_upto_exclusive = i;
 
-        println!("r_from: {} r_upto: {}", r_from, r_upto_exclusive);
-        println!("l_from: {} l_upto: {}", l_from, l_upto_exclusive);
+        // println!("r_from: {} r_upto: {}", r_from, r_upto_exclusive);
+        // println!("l_from: {} l_upto: {}", l_from, l_upto_exclusive);
 
         let has_r_events = r_upto_exclusive > r_from;
         let has_l_events = l_upto_exclusive > l_from;
@@ -83,38 +82,30 @@ where
             let r_upto = r_upto_exclusive - 1;
             // Connect elements in [r_from, r_upto) to higher index
             for j in r_from .. r_upto {
-                map[j as usize] = (j + 1) as usize;
+                map[j] = j + 1;
             }
             // Special handling of *last* element: Connect either the last L event
             // or loop back to start of R events (if no L events).
             if has_l_events {
-                map[r_upto as usize] = (l_upto_exclusive - 1) as usize;
+                map[r_upto] = l_upto_exclusive - 1;
             } else {
-                map[r_upto as usize] = r_from as usize;
+                map[r_upto] = r_from;
             }
         }
         if has_l_events {
             let l_upto = l_upto_exclusive - 1;
             // Connect elements in (l_from, l_upto] to lower index
             for j in l_from + 1 ..= l_upto {
-                map[j as usize] = (j - 1) as usize;
+                map[j] = j - 1;
             }
             // Special handling of *first* element: Connect either to the first R event
             // or loop back to end of L events (if no R events).
             if has_r_events {
-                map[l_from as usize] = r_from as usize;
+                map[l_from] = r_from;
             } else {
-                map[l_from as usize] = l_upto as usize;
+                map[l_from] = l_upto;
             }
         }
-
-
-        /*
-        for j in r_from .. r_upto {
-            map[j as usize] = (j + 1) as usize;
-        }
-        map[r_upto as usize] = r_from as usize;
-        */
     }
 
     map
@@ -248,7 +239,7 @@ where
 {
     let result_events = order_events(sorted_events);
 
-    build_vertex_iteration_order(&result_events, |a, b| a.point == b.point, |e| e.is_left());
+    precompute_iteration_order(&result_events, |a, b| a.point == b.point, |e| e.is_left());
 
     #[cfg(feature = "debug-booleanop")]
     write_debug_csv(&result_events);
@@ -344,7 +335,7 @@ where
 }
 
 #[cfg(test)]
-mod test_check_precompute_order {
+mod test_precompute_iteration_order {
     use super::*;
     use LeftRight::*;
 
@@ -362,14 +353,14 @@ mod test_check_precompute_order {
             let expected: &[usize] = &$expected;
             println!("\nData: {:?}\nExpected: {:?}", data, expected);
             assert_eq!(
-                build_vertex_iteration_order(&data, |a, b| a.0 == b.0, |(_, lr)| **lr == L),
+                precompute_iteration_order(&data, |a, b| a.0 == b.0, |(_, lr)| **lr == L),
                 expected,
             );
         };
     }
 
     #[test]
-    fn test_build_vertex_iteration_order_right_events() {
+    fn test_precompute_iteration_order_right_events() {
         check!([], [], []);
         check!([1], [R], [0]);
         check!([1, 2, 3], [R, R, R], [0, 1, 2]);
@@ -381,7 +372,7 @@ mod test_check_precompute_order {
     }
 
     #[test]
-    fn test_build_vertex_iteration_order_left_events() {
+    fn test_precompute_iteration_order_left_events() {
         check!([], [], []);
         check!([1], [L], [0]);
         check!([1, 2, 3], [L, L, L], [0, 1, 2]);
@@ -394,7 +385,7 @@ mod test_check_precompute_order {
     }
 
     #[test]
-    fn test_build_vertex_iteration_order_mixed_events() {
+    fn test_precompute_iteration_order_mixed_events() {
         check!([1, 2], [R, L], [0, 1]);
         check!([1, 2], [L, R], [0, 1]);
 
@@ -406,6 +397,12 @@ mod test_check_precompute_order {
         check!([1, 1, 1, 2, 2, 2], [L, L, L, R, R, R], [2, 0, 1, 4, 5, 3]);
 
         check!([1, 1, 1, 1, 1, 1], [R, R, R, L, L, L], [1, 2, 5, 0, 3, 4]);
+
+        check!(
+            [1, 1, 1, 1, 2, 2, 3, 3, 3, 3],
+            [R, R, L, L, R, L, R, R, L, L],
+            [1, 3, 0, 2, 5, 4, 7, 9, 6, 8]
+        );
     }
 
 }
