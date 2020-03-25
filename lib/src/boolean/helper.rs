7ca89bf9499e1425b4ab23aeec53e1b617769abe
Bronson Brown-deVost <bronsonbdevost@aim.com>
2020-03-25 20:30:16 +0100

    removed libc

--- lib/src/boolean/helper.rs
+++ lib/src/boolean/helper.rs
@@ -1,14 +1,45 @@
-use libc::{c_double, c_float};
+use float_extras::f64::nextafter;
 use num_traits::Float as NumTraitsFloat;
 use std::cmp::Ordering;
 use std::fmt::{Debug, Display};
 use std::{f32, f64};
 
-#[link_name = "m"]
-extern "C" {
-    pub fn nextafter(x: c_double, y: c_double) -> c_double;
-    pub fn nextafterf(x: c_float, y: c_float) -> c_float;
-}
+// TODO: reimpliment the f32 version of nextafter(f) if possible
+
+// pub trait Float: NumTraitsFloat + Debug + Display + NextAfter + Into<f64> {}
+
+// impl<T: NumTraitsFloat + Debug + Display + NextAfter + Into<f64>> Float for T {}
+
+// pub trait NextAfter: NumTraitsFloat {
+//     fn nextafter(self, up: bool) -> Self;
+// }
+
+// /// nextafter returns the next representable floating-point value following x in the
+// /// direction of y. If y is less than x, these functions will return the largest representable
+// /// number less than x.
+// pub fn nextafterf(x: f32, y: f32) -> f32 {
+//     unsafe { cmath::nextafter(x, y) }
+// };
+
+// impl NextAfter for f64 {
+//     fn nextafter(self, up: bool) -> Self {
+//         if up {
+//             unsafe { nextafter(self, std::f64::INFINITY) }
+//         } else {
+//             unsafe { nextafter(self, std::f64::NEG_INFINITY) }
+//         }
+//     }
+// }
+
+// impl NextAfter for f32 {
+//     fn nextafter(self, up: bool) -> Self {
+//         if up {
+//             unsafe { nextafterf(self, std::f32::INFINITY) }
+//         } else {
+//             unsafe { nextafterf(self, std::f32::NEG_INFINITY) }
+//         }
+//     }
+// }
 
 pub trait Float: NumTraitsFloat + Debug + Display + NextAfter + Into<f64> {}
 
@@ -16,25 +47,32 @@ impl<T: NumTraitsFloat + Debug + Display + NextAfter + Into<f64>> Float for T {}
 
 pub trait NextAfter: NumTraitsFloat {
     fn nextafter(self, up: bool) -> Self;
+    fn nextafter_steps(self, steps: i32) -> Self;
+
+    fn ulp(self) -> Self {
+        if self > Self::zero() {
+            self.nextafter(true) - self
+        } else {
+            self.nextafter(false) - self
+        }
+    }
 }
 
 impl NextAfter for f64 {
     fn nextafter(self, up: bool) -> Self {
         if up {
-            unsafe { nextafter(self, std::f64::INFINITY) }
+            nextafter(self, std::f64::INFINITY)
         } else {
-            unsafe { nextafter(self, std::f64::NEG_INFINITY) }
+            nextafter(self, std::f64::NEG_INFINITY)
         }
     }
-}
 
-impl NextAfter for f32 {
-    fn nextafter(self, up: bool) -> Self {
-        if up {
-            unsafe { nextafterf(self, std::f32::INFINITY) }
-        } else {
-            unsafe { nextafterf(self, std::f32::NEG_INFINITY) }
+    fn nextafter_steps(self, steps: i32) -> Self {
+        let mut x = self;
+        for _ in 0..steps.abs() {
+            x = x.nextafter(steps > 0);
         }
+        x
     }
 }
 
@@ -58,7 +96,7 @@ pub fn less_if_inversed(condition: bool) -> Ordering {
 
 #[cfg(test)]
 pub mod test {
-    use super::{nextafter, nextafterf, Float};
+    use super::{nextafter, Float};
     use geo_types::Coordinate;
 
     pub fn xy<X: Into<f64>, Y: Into<f64>>(x: X, y: Y) -> Coordinate<f64> {
@@ -78,6 +116,6 @@ pub mod test {
         }
 
         assert_eq!(dummy(0_f64), unsafe { nextafter(0_f64, std::f64::INFINITY) });
-        assert_eq!(dummy(0_f32), unsafe { nextafterf(0_f32, std::f32::INFINITY) });
+        //assert_eq!(dummy(0_f32), unsafe { nextafterf(0_f32, std::f32::INFINITY) });
     }
 }
