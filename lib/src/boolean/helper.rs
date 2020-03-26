e39dbedf5ece840da6c1c4dbb019282ffe85ffa5
Bronson Brown-deVost <bronsonbdevost@aim.com>
2020-03-26 02:54:41 +0100

    cleanup, comments, and generic nextafter

--- lib/src/boolean/helper.rs
+++ lib/src/boolean/helper.rs
@@ -3,103 +3,73 @@ use std::cmp::Ordering;
 use std::fmt::{Debug, Display};
 use std::{f32, f64};
 
-fn nextafter(x: f64, y: f64) -> f64 {
+fn nextafter<T>(x: T, y: T) -> T 
+where 
+    T: NumTraitsFloat
+{
     if y == x {
         return y;
     }         
 
-    if x >= f64::INFINITY {
-        return f64::INFINITY;
+    if x >= T::infinity() {
+        return T::infinity();
     }
         
-
-    if x <= f64::NEG_INFINITY {
-        return f64::NEG_INFINITY;
+    if x <= T::neg_infinity() {
+        return T::neg_infinity();
     }
 
-
-    if -1.0 <= x && x <= 1.0 {
-        dbg!("Hit the min/max if");
+    if T::from(-1.0).unwrap() <= x && x <= T::from(1.0).unwrap() {
         if y > x {
-            return x + f64::EPSILON;
+            return x + T::epsilon();
         }    
         else {
-            return x - f64::EPSILON;
+            return x - T::epsilon();
         }
     }
     
-    
-    dbg!("Destructuring the float");
-    let (m, e, s) = integer_decode(x);        
+    let (m, e, s) = x.integer_decode();        
     if y > x {
         let adj_m = m + 1;
-        return (s as f64) * (adj_m as f64) * (2f64.powf(e as f64)) as f64
+        return T::from(s).unwrap() * T::from(adj_m).unwrap() * T::from(2f64).unwrap().powf(T::from(e).unwrap())
     }
     else {
         let adj_m = m - 1;
-        return (s as f64) * (adj_m as f64) * (2f64.powf(e as f64)) as f64
+        return T::from(s).unwrap() * T::from(adj_m).unwrap() * T::from(2f64).unwrap().powf(T::from(e).unwrap())
     }
 }
 
-fn integer_decode(val: f64) -> (u64, i16, i8) {
-    let bits = val.to_bits();
-    let sign: i8 = if bits >> 63 == 0 { 1 } else { -1 };
-    let mut exponent: i16 = ((bits >> 52) & 0x7ff) as i16;
-    let mantissa = if exponent == 0 {
-        (bits & 0xfffffffffffff) << 1
-    } else {
-        (bits & 0xfffffffffffff) | 0x10000000000000
-    };
-    exponent -= 1023 + 52;
-    (mantissa, exponent, sign)
+trait IntegerDecode {
+    fn integer_decode(&self) -> (u64, i16, i8);
 }
 
-fn nextafterf(x: f32, y: f32) -> f32 {
-    if y == x {
-        return y;
-    }         
-
-    if x >= f32::INFINITY {
-        return f32::INFINITY;
-    }
-        
-
-    if x <= f32::NEG_INFINITY {
-        return f32::NEG_INFINITY;
-    }
-
-
-    if -1.0 <= x && x <= 1.0 {
-        dbg!("Hit the min/max if");
-        if y > x {
-            return x + f32::EPSILON;
-        }    
-        else {
-            return x - f32::EPSILON;
-        }
-    }
-    
-    
-    dbg!("Destructuring the float");
-    let (m, e, s) = integer_decodef(x);        
-    if y > x {
-        let adj_m = m + 1;
-        return (s as f32) * (adj_m as f32) * (2f32.powf(e as f32)) as f32
-    }
-    else {
-        let adj_m = m - 1;
-        return (s as f32) * (adj_m as f32) * (2f32.powf(e as f32)) as f32
+impl IntegerDecode for f64 {
+    // See https://github.com/rust-lang/rust/blob/master/src/libcore/num/dec2flt/rawfp.rs
+    fn integer_decode(&self) -> (u64, i16, i8) {
+        let bits = self.to_bits();
+        let sign: i8 = if bits >> 63 == 0 { 1 } else { -1 };
+        let mut exponent: i16 = ((bits >> (f64::MANTISSA_DIGITS - 1)) & 0x7ff) as i16;
+        let mantissa = if exponent == 0 {
+            (bits & 0xfffffffffffff) << 1
+        } else {
+            (bits & 0xfffffffffffff) | 0x10000000000000
+        };
+        exponent -= 1023 + (f64::MANTISSA_DIGITS as i16 - 1);
+        (mantissa, exponent, sign)
     }
 }
 
-fn integer_decodef(val: f32) -> (u32, i16, i8) {
-    let bits = val.to_bits();
-    let sign: i8 = if bits >> 31 == 0 { 1 } else { -1 };
-    let mut exponent: i16 = ((bits >> 23) & 0xff) as i16;
-    let mantissa =
-        if exponent == 0 { (bits & 0x7fffff) << 1 } else { (bits & 0x7fffff) | 0x800000 };
-    exponent -= 127 + 23;
-    (mantissa, exponent, sign)
+impl IntegerDecode for f32 {
+    // See https://github.com/rust-lang/rust/blob/master/src/libcore/num/dec2flt/rawfp.rs
+    fn integer_decode(&self) -> (u64, i16, i8) {
+        let bits = self.to_bits();
+        let sign: i8 = if bits >> 31 == 0 { 1 } else { -1 };
+        let mut exponent: i16 = ((bits >> (f32::MANTISSA_DIGITS - 1)) & 0xff) as i16;
+        let mantissa =
+            if exponent == 0 { (bits & 0x7fffff) << 1 } else { (bits & 0x7fffff) | 0x800000 };
+        exponent -= 127 + (f32::MANTISSA_DIGITS as i16 - 1);
+        (mantissa as u64, exponent, sign)
+    }
 }
 
 pub trait Float: NumTraitsFloat + Debug + Display + NextAfter + Into<f64> {}
@@ -123,9 +93,9 @@ impl NextAfter for f64 {
 impl NextAfter for f32 {
     fn nextafter(self, up: bool) -> Self {
         if up {
-            nextafterf(self, std::f32::INFINITY)
+            nextafter(self, std::f32::INFINITY)
         } else {
-            nextafterf(self, std::f32::NEG_INFINITY)
+            nextafter(self, std::f32::NEG_INFINITY)
         }
     }
 }
@@ -150,7 +120,7 @@ pub fn less_if_inversed(condition: bool) -> Ordering {
 
 #[cfg(test)]
 pub mod test {
-    use super::{nextafter, nextafterf, Float};
+    use super::{nextafter, Float};
     use geo_types::Coordinate;
 
     pub fn xy<X: Into<f64>, Y: Into<f64>>(x: X, y: Y) -> Coordinate<f64> {
@@ -169,7 +139,7 @@ pub mod test {
             x.nextafter(true)
         }
 
-        assert_eq!(dummy(0_f64), unsafe { nextafter(0_f64, std::f64::INFINITY) });
-        assert_eq!(dummy(0_f32), unsafe { nextafterf(0_f32, std::f32::INFINITY) });
+        assert_eq!(dummy(0_f64), nextafter(0_f64, std::f64::INFINITY));
+        assert_eq!(dummy(0_f32), nextafter(0_f32, std::f32::INFINITY));
     }
 }
