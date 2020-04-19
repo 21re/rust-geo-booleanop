5928c23c6ab1eaa29d58cb30a71bcee8b6466cdb
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-04-19 18:38:37 +0200

    added data generator for circular data

--- tests/src/bin/run_on_generated_dataset.rs
+++ tests/src/bin/run_on_generated_dataset.rs
@@ -7,8 +7,8 @@ use geojson::Feature;
 use geo_booleanop::boolean::BooleanOp;
 
 use geo_booleanop_tests::compact_geojson::write_compact_geojson;
-use geo_booleanop_tests::helper::{TestOperation, apply_operation, extract_expected_result, load_test_case, convert_to_feature};
-use geo_booleanop_tests::data_generators::{generate_grid};
+use geo_booleanop_tests::helper::{TestOperation, apply_operation, extract_expected_result, load_test_case, convert_to_feature, xy};
+use geo_booleanop_tests::data_generators::{generate_grid, generate_concentric_circles};
 
 use std::fs;
 use std::path::Path;
@@ -37,7 +37,21 @@ fn main() {
         convert_to_feature(&a, None),
         convert_to_feature(&b, None),
         convert_to_feature(&result, Some(op)),
-    ], "newtest.geoson");
+    ], "newtest_grid.geoson");
+
+
+    let a = generate_concentric_circles(xy(0, 0), 1.0, 10.0, 2, 8);
+    let b = generate_concentric_circles(xy(5, 5), 1.0, 10.0, 20, 100);
+
+    let op = TestOperation::Xor;
+
+    let result = apply_operation(&a, &b, op);
+
+    write_compact_geojson(&[
+        convert_to_feature(&a, None),
+        convert_to_feature(&b, None),
+        convert_to_feature(&result, Some(op)),
+    ], "newtest_circle.geoson");
 
     //write_testcase(&[grid1, grid2, xor], "newtest.geoson");
 }
--- tests/src/data_generators.rs
+++ tests/src/data_generators.rs
@@ -8,7 +8,8 @@ use std::iter::FromIterator;
 use super::helper::xy;
 use super::compact_geojson::write_compact_geojson;
 
-pub fn generate_rect_centered(center: Coordinate<f64>, w: f64, h: f64) -> Polygon<f64> {
+
+fn generate_rect_centered(center: Coordinate<f64>, w: f64, h: f64) -> Polygon<f64> {
     let w_half = w / 2.0;
     let h_half = h / 2.0;
     Polygon::new(
@@ -23,6 +24,20 @@ pub fn generate_rect_centered(center: Coordinate<f64>, w: f64, h: f64) -> Polygo
     )
 }
 
+fn generate_circle_ring(center: Coordinate<f64>, num_points: usize, r: f64) -> LineString<f64> {
+    let mut coords = Vec::with_capacity(num_points);
+
+    for i in 0 .. num_points {
+        let phi = (i as f64) / (num_points as f64) * 2.0 * std::f64::consts::PI;
+        coords.push(xy(
+            center.x + r * phi.sin(),
+            center.y + r * phi.cos(),
+        ));
+    }
+
+    LineString(coords)
+}
+
 pub fn generate_grid(min: f64, max: f64, rect_size: f64, num_rects: i32) -> MultiPolygon<f64> {
     assert!(num_rects >= 2);
 
@@ -35,6 +50,29 @@ pub fn generate_grid(min: f64, max: f64, rect_size: f64, num_rects: i32) -> Mult
         for y in &positions {
             polygons.push(generate_rect_centered(Coordinate{x: *x, y: *y}, rect_size, rect_size));
         }
+    }
+
+    MultiPolygon(polygons)
+}
+
+pub fn generate_concentric_circles(center: Coordinate<f64>, r_min: f64, r_max: f64, num_polys: usize, num_points: usize) -> MultiPolygon<f64> {
+    assert!(r_max > r_min);
+    assert!(r_min > 0.0);
+    assert!(num_polys >= 1);
+    assert!(num_points >= 3);
+
+    let num_radii = 2 * num_polys;   // We need 2*n radii in total
+    let radii: Vec<_> = (0 .. num_radii).map(
+        |i| r_min + (i as f64) * (r_max - r_min) / ((num_radii - 1) as f64)
+    ).collect();
+
+    let mut polygons = Vec::new();
+    for i in (0 .. num_radii).step_by(2) {
+        let ring1 = generate_circle_ring(center, num_points, radii[i]);
+        let ring2 = generate_circle_ring(center, num_points, radii[i + 1]);
+        polygons.push(
+            Polygon::new(ring1, vec![ring2])
+        )
     }
 
     MultiPolygon(polygons)
